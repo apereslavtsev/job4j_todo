e0448eb26a8c953198cc7f8af6d74b9aae00d8f7
apereslavtsev <pereslavtsev.anton@gmail.com>
2023-06-16 19:36:19 +0300

    5. Аутентификация в проекте TODO [#504877 # [#504877 #423401]]

--- db/scripts/001_ddl_create_initial_schema.sql
+++ db/scripts/001_ddl_create_initial_schema.sql
@@ -1,14 +1,14 @@
+CREATE TABLE todo_user (
+    id       SERIAL PRIMARY KEY,
+    name     varchar        not null,
+    login    varchar unique not null,
+    password varchar not null
+);
+
 CREATE TABLE tasks (
    id SERIAL PRIMARY KEY,
    user_id int references todo_user(id),
    description TEXT,
    created TIMESTAMP,
    done BOOLEAN
-);
-
-CREATE TABLE todo_user (
-    id       SERIAL PRIMARY KEY,
-    name     varchar        not null,
-    login    varchar unique not null,
-    password varchar not null
 );
