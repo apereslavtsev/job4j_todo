fd282630fa7ff8c4d62b78a7d499092c91fc15f0
apereslavtsev <pereslavtsev.anton@gmail.com>
2023-06-17 17:21:03 +0300

    6. Лямбды и шаблон Command [#49295 # [#49295]]

--- src/main/java/ru/job4j/todo/repository/HibernateTaskStore.java
+++ src/main/java/ru/job4j/todo/repository/HibernateTaskStore.java
@@ -1,14 +1,13 @@
 package ru.job4j.todo.repository;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Task;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
@@ -17,18 +16,13 @@ public class HibernateTaskStore implements TaskRepository {
 
     private static final Logger LOG = LoggerFactory.getLogger(HibernateTaskStore.class.getName());
 
-    private final SessionFactory sf;
+    private final CrudRepository crudRepository;
 
     @Override
     public Task create(Task task) {
-        Session session = sf.openSession();
         try {
-            session.beginTransaction();
-            session.save(task);
-            session.getTransaction().commit();
-            session.close();
+            crudRepository.run(session -> session.save(task));
         } catch (Exception e) {
-            session.getTransaction().rollback();
             LOG.error("save tasks", e);
         }
         return task;
@@ -36,16 +30,11 @@ public class HibernateTaskStore implements TaskRepository {
 
     @Override
     public boolean update(Task task) {
-        Session session = sf.openSession();
         boolean rsl = false;
         try {
-            session.beginTransaction();
-            session.update(task);
-            session.getTransaction().commit();
-            session.close();
+            crudRepository.run(session -> session.merge(task));
             rsl = true;
         } catch (Exception e) {
-            session.getTransaction().rollback();
             LOG.error("update tasks", e);
         }
         return rsl;
@@ -53,19 +42,11 @@ public class HibernateTaskStore implements TaskRepository {
 
     @Override
     public boolean done(int id) {
-        Session session = sf.openSession();
         boolean rsl = false;
         try {
-            session.beginTransaction();
-            rsl = session.createQuery(
-                    "update Task set done = :doneParam where id = :idParam")
-                    .setParameter("doneParam", true)
-                    .setParameter("idParam" ,id)
-                    .executeUpdate() > 0;
-            session.getTransaction().commit();
-            session.close();
+            rsl = crudRepository.run("update Task set done = :done where id = :id",
+                    Map.of("done", true, "id", id)) > 0;
         } catch (Exception e) {
-            session.getTransaction().rollback();
             LOG.error("done tasks", e);
         }
         return rsl;
@@ -73,34 +54,22 @@ public class HibernateTaskStore implements TaskRepository {
 
     @Override
     public boolean delete(int taskId) {
-        Session session = sf.openSession();
         boolean rsl = false;
         try {
-            session.beginTransaction();
-            var task = new Task();
-            task.setId(taskId);
-            session.delete(task);
-            session.getTransaction().commit();
-            session.close();
-            rsl = true;
+            rsl = crudRepository.run("delete from Task where id =:id",
+                    Map.of("id", taskId)) > 0;
         } catch (Exception e) {
-            session.getTransaction().rollback();
-            LOG.error("delete tasks", e);
+            LOG.error("delete task", e);
         }
         return rsl;
     }
 
     @Override
     public List<Task> findAllOrderById() {
-        Session session = sf.openSession();
         List<Task> result = List.of();
         try {
-            session.beginTransaction();
-            result = session.createQuery("from Task order by id", Task.class).list();
-            session.getTransaction().commit();
-            session.close();
+            result = crudRepository.query("from Task order by id", Task.class);
         } catch (Exception e) {
-            session.getTransaction().rollback();
             LOG.error("find all tasks", e);
         }
         return result;
@@ -108,17 +77,11 @@ public class HibernateTaskStore implements TaskRepository {
 
     @Override
     public List<Task> getByDoneOrderById(Boolean done) {
-        Session session = sf.openSession();
         List<Task> result = List.of();
         try {
-            session.beginTransaction();
-            result = session.createQuery(
-                            "from Task as t where t.done = :fDone order by id", Task.class)
-                    .setParameter("fDone", done).list();
-            session.getTransaction().commit();
-            session.close();
+            result = crudRepository.query("from Task as t where t.done = :fDone order by id",
+                    Task.class, Map.of("fDone", done));
         } catch (Exception e) {
-            session.getTransaction().rollback();
             LOG.error("find tasks by done", e);
         }
         return result;
@@ -126,18 +89,12 @@ public class HibernateTaskStore implements TaskRepository {
 
     @Override
     public Optional<Task> findById(int taskId) {
-        Session session = sf.openSession();
-        Optional<Task> result = Optional.empty();
-        try {
-            session.beginTransaction();
-            result = Optional.of(session.get(Task.class, taskId));
-            session.getTransaction().commit();
-            session.close();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            LOG.error("find tasks by id", e);
-        }
-        return result;
+
+        return crudRepository.optional(
+                "from Task where id = :fId", Task.class,
+                Map.of("fId", taskId)
+        );
+
     }
 
 }
